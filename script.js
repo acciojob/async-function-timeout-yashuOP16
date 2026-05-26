fad89ecdc99eaf99f86267251ad6caddaaff652e
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-05-26 16:10:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+const button = document.getElementById("btn");
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const output = document.getElementById("output");
+
+function delayMessage(time) {
+	return new Promise((resolve , reject) => {
+		setTimeout(() => {
+			resolve();
+		}, time)
+	})
+}
+
+async function showMessage() {
+	const text = text.innerText.value;
+	const delay = Number(delayInput.value);
+	output.innerText = "";
+	await delayMessage(delay);
+	output.innerText = text;
+}
+
+button.addEventListener("click" , () => {
+	showMessage;
+})
